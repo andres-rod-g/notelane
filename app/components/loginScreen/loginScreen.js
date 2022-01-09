7a400dd221ac2a560ffdb064fbde6405e8b1e0a6
Andrés Rodríguez <79536152+andres-rod-g@users.noreply.github.com>
2022-01-08 19:32:37 -0500

    Before Ejecting

--- app/components/loginScreen/loginScreen.js
+++ app/components/loginScreen/loginScreen.js
@@ -1,8 +1,31 @@
-import { Image, StatusBar, Text, View, Button, TouchableOpacityComponent, TouchableOpacity } from 'react-native';
-
 import Styles from './styles'
 
+import { useState } from 'react';
+import { Image, StatusBar, Text, View, Button, TouchableOpacityComponent, TouchableOpacity } from 'react-native';
+import {
+    GoogleSignin,
+    GoogleSigninButton
+  } from '@react-native-google-signin/google-signin';
+
 export default () => {
+    const [userInfo, setUserInfo] = useState(null)
+
+    GoogleSignin.configure({
+        webClientId: '71825131047-g77sla1hs6hl3fo3m533kf2ppes9la4v.apps.googleusercontent.com',
+        offlineAccess: true,
+        forceCodeForRefreshToken: true
+    })
+
+    const singIn = async () => {
+        try {
+            await GoogleSignin.hasPlayServices();
+            const userInfo = await GoogleSignin.signIn();
+            setUserInfo({userInfo})
+        } catch (error) {
+            if (error) return console.log(error)
+        }
+    }
+
     return (
         <View style={Styles.container}>
             <View style={Styles.titleContainer}>
@@ -11,6 +34,7 @@ export default () => {
                 </View>
                 <Text style={Styles.title}>Notelane</Text>
             </View>
+            <GoogleSigninButton size={GoogleSigninButton.Size.Wide} color={GoogleSigninButton.Color.Dark}/>
             <TouchableOpacity style={Styles.loginButton}>
                 <Text style={{color: '#E54B4B'}}>
                     Log In with Google
--- app/react-native.config.js
+++ app/react-native.config.js
@@ -1,3 +1,10 @@
 module.exports = {
     assets: ['./assets/fonts'],
+    dependencies: {
+      'react-native-google-signin': {
+        platforms: {
+          android: null
+        }
+      }
+    }
   };
